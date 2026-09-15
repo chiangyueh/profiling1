f162b23183981564e4711aebf8c44a7ecd122e7f
chiangyueh <113624708+chiangyueh@users.noreply.github.com>
2026-09-16 01:01:40 +0300

    Update run_npu.sh

--- run_npu.sh
+++ run_npu.sh
@@ -11,4 +11,13 @@ cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 source /usr/local/Ascend/cann-8.5.0/set_env.sh
 export ASCEND_RT_VISIBLE_DEVICES=2
 
-exec bash build.sh --run_example mat_mul_v3 eager --example_name=matmul
+for shrink_mode in 0 1; do
+    echo "CORE_SHRINK_RUN_BEGIN enabled=${shrink_mode}"
+
+    MATMUL_V3_SHRINK_IDLE_CORES="${shrink_mode}" \
+        bash build.sh \
+        --run_example mat_mul_v3 eager \
+        --example_name=matmul
+
+    echo "CORE_SHRINK_RUN_END enabled=${shrink_mode}"
+done
